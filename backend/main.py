5d1966cc9ca69aabf9c0008388761a5d5ad29597
Aryan Nasina <aryannasina2@gmail.com>
2026-07-19 11:15:34 +0530

    Prepare for deployment

--- backend/main.py
+++ backend/main.py
@@ -92,16 +92,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        settings.FRONTEND_URL,
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5175",
-        "http://localhost:5176",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"]
 )
